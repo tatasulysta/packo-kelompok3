9298f1008ea474ab0b8a7bbda3575891e8a95913
Gerrad <gerrad.natanael@ui.ac.id>
2022-09-08 22:14:13 +0700

    edit layout

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,17 +3,19 @@ import styles from './styles.module.css';
 
 function Menu(props) {
   return (
-    <div>
-      <h1>{props.title}</h1>
-      {props.data.map((menu) => {
-        return (
-          <>
-            <p>{menu.nama}</p>
-            <p>{menu.harga}</p>
-            <img src={menu.gambar} />
-          </>
-        );
-      })}
+    <div className={styles.category}>
+      <h3>{props.title}</h3>
+      <div className={styles.menu}>
+        {props.data.map((menu) => {
+          return (
+            <div>
+              <p>{menu.nama}</p>
+              <p>{menu.harga}</p>
+              <img src={menu.gambar} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
