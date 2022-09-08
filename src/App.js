0f879ebf639fc900bb722e829adfa64770cb5677
Tata Sulysta <tata_ss@softwareseni.com>
2022-09-08 15:41:05 +0700

    Add breadcrumbs component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Rating from './components/Rating';
 import Navbar from './components/Navbar';
+import Breadcrumbs from './components/Breadcrumbs';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Rating />
+        <Breadcrumbs />
         <Navbar />
       </header>
     </div>
--- src/components/Breadcrumbs/index.jsx
+++ src/components/Breadcrumbs/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdOutlineNavigateNext } from 'react-icons/md';
+import styles from './styles.module.css';
 
 const Breadcrumbs = () => {
   const datas = [
@@ -18,11 +21,33 @@ const Breadcrumbs = () => {
       link: '/buburyoyong'
     }
   ];
+  const isLastIndex = (index) => {
+    return index === datas.length - 1;
+  };
   return (
     <nav>
-      <ul>
-        {datas.map((data) => {
-          return <li>{data.name}</li>;
+      <ul className={styles.wrapper}>
+        {datas.map((data, index) => {
+          return (
+            <li key={data.id} className={styles.navItem}>
+              {isLastIndex(index) ? (
+                <Link
+                  to={data.link}
+                  className={`text-xs text-weight-medium ${styles.link} ${styles.lastItem}`}>
+                  {data.name}
+                </Link>
+              ) : (
+                <div className={styles.linkWrapper}>
+                  <Link
+                    to={data.link}
+                    className={`text-xs text-weight-medium ${styles.link} ${styles.item}`}>
+                    {data.name}
+                  </Link>
+                  <MdOutlineNavigateNext className={styles.icon} />
+                </div>
+              )}
+            </li>
+          );
         })}
       </ul>
     </nav>
