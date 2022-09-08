119778c2c96edc07b4ba378a64301320d44cfee3
Gerrad <gerrad.natanael@ui.ac.id>
2022-09-08 21:30:05 +0700

    add menu category

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Menu from './components/Menu';
+import { data } from './data/dummyData';
 
 function App() {
   return (
@@ -11,6 +13,13 @@ function App() {
       <Header>
         <Navbar />
       </Header>
+      <main>
+        <Menu data={data.bubur} title="Bubur" />
+        <Menu data={data.nasi} title="Nasi" />
+        <Menu data={data.miAyam} title="Mi Ayam" />
+        <Menu data={data.snack} title="Snack" />
+        <Menu data={data.minuman} title="Minuman" />
+      </main>
       <Footer />
     </>
   );
