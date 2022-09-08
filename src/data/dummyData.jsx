ec5552ebbad6f336f25ca266a439ed754427000f
Gerrad <gerrad.natanael@ui.ac.id>
2022-09-08 21:31:11 +0700

    create dummy data

---
+++ src/data/dummyData.jsx
@@ -0,0 +1,146 @@
+import React from 'react';
+import bubur1 from '../assets/menus/bubur1.png';
+import bubur2 from '../assets/menus/bubur2.png';
+import bubur3 from '../assets/menus/bubur3.png';
+import nasi1 from '../assets/menus/nasi1.png';
+import miAyam1 from '../assets/menus/miAyam1.png';
+import snack1 from '../assets/menus/snack1.png';
+import minuman1 from '../assets/menus/minuman1.png';
+
+export const data = {
+  bubur: [
+    {
+      nama: 'Bubur Cakwe Telor Campur',
+      harga: '28.000',
+      gambar: bubur1
+    },
+    {
+      nama: 'Bubur Manja',
+      harga: '28.000',
+      gambar: bubur1
+    },
+    {
+      nama: 'Bubur Teknik',
+      harga: '28.000',
+      gambar: bubur2
+    },
+    {
+      nama: 'Bubur Mewah',
+      harga: '28.000',
+      gambar: bubur3
+    },
+    {
+      nama: 'Bubur Aja',
+      harga: '28.000',
+      gambar: bubur2
+    }
+  ],
+  nasi: [
+    {
+      nama: 'Nasi Cakwe Telor Campur',
+      harga: '28.000',
+      gambar: nasi1
+    },
+    {
+      nama: 'Nasi Manja',
+      harga: '28.000',
+      gambar: nasi1
+    },
+    {
+      nama: 'Nasi Teknik',
+      harga: '28.000',
+      gambar: nasi1
+    },
+    {
+      nama: 'Nasi Mewah',
+      harga: '28.000',
+      gambar: nasi1
+    },
+    {
+      nama: 'Nasi Aja',
+      harga: '28.000',
+      gambar: nasi1
+    }
+  ],
+  miAyam: [
+    {
+      nama: 'Mi Ayam Cakwe Telor Campur',
+      harga: '28.000',
+      gambar: miAyam1
+    },
+    {
+      nama: 'Mi Ayam Manja',
+      harga: '28.000',
+      gambar: miAyam1
+    },
+    {
+      nama: 'Mi Ayam Teknik',
+      harga: '28.000',
+      gambar: miAyam1
+    },
+    {
+      nama: 'Mi Ayam Mewah',
+      harga: '28.000',
+      gambar: miAyam1
+    },
+    {
+      nama: 'Mi Ayam Aja',
+      harga: '28.000',
+      gambar: miAyam1
+    }
+  ],
+  snack: [
+    {
+      nama: 'Snack Cakwe Telor Campur',
+      harga: '28.000',
+      gambar: snack1
+    },
+    {
+      nama: 'Snack Manja',
+      harga: '28.000',
+      gambar: snack1
+    },
+    {
+      nama: 'Snack Teknik',
+      harga: '28.000',
+      gambar: snack1
+    },
+    {
+      nama: 'Snack Mewah',
+      harga: '28.000',
+      gambar: snack1
+    },
+    {
+      nama: 'Snack Aja',
+      harga: '28.000',
+      gambar: snack1
+    }
+  ],
+  minuman: [
+    {
+      nama: 'Minuman Cakwe Telor Campur',
+      harga: '28.000',
+      gambar: minuman1
+    },
+    {
+      nama: 'Minuman Manja',
+      harga: '28.000',
+      gambar: minuman1
+    },
+    {
+      nama: 'Minuman Teknik',
+      harga: '28.000',
+      gambar: minuman1
+    },
+    {
+      nama: 'Minuman Mewah',
+      harga: '28.000',
+      gambar: minuman1
+    },
+    {
+      nama: 'Minuman Aja',
+      harga: '28.000',
+      gambar: minuman1
+    }
+  ]
+};
