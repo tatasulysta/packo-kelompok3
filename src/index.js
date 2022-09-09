1fea28042f015984a5b551797191e44becd229d8
Gerrad <gerrad.natanael@ui.ac.id>
2022-09-09 12:58:55 +0700

    add menu context provider

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import ContextProvider from './contexts/Context';
+import MenuContextProvider from './contexts/menuContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ContextProvider>
-        <App />
+        <MenuContextProvider>
+          <App />
+        </MenuContextProvider>
       </ContextProvider>
     </BrowserRouter>
   </React.StrictMode>
