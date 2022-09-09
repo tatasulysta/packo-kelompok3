77553fc4c27add626e694b4065e23d40d1f50f9e
Julian_A_Wardana <julian.wardana@binus.ac.id>
2022-09-09 11:09:42 +0700

    refactor: move all images to folder images

--- src/data/food.js
+++ src/data/food.js
@@ -1,25 +1,25 @@
 const foodRecommendations = [
   {
     id: 1,
-    imgUrl: '/menu/bubur/medium/bubur-sarang-telor-setengah-matang.png',
+    imgUrl: '/images/porridge/medium/bubur-sarang-telor-setengah-matang.png',
     name: 'Bubur Sarang Telor Setengah Matang',
     price: 32000
   },
   {
     id: 2,
-    imgUrl: '/menu/bubur/medium/bubur-cakwe-telor-campur.png',
+    imgUrl: '/images/porridge/medium/bubur-cakwe-telor-campur.png',
     name: 'Bubur Cakwe Telor Campur',
     price: 32000
   },
   {
     id: 3,
-    imgUrl: '/menu/bubur/medium/bubur-ayam-campur.png',
+    imgUrl: '/images/porridge/medium/bubur-ayam-campur.png',
     name: 'Bubur Ayam Jamur',
     price: 28000
   },
   {
     id: 4,
-    imgUrl: '/menu/bubur/medium/bubur-sarang-telor-kepo.png',
+    imgUrl: '/images/porridge/medium/bubur-sarang-telor-kepo.png',
     name: 'Bubur Telor Sarang Kepo',
     price: 28000
   }
@@ -28,31 +28,31 @@ const foodRecommendations = [
 const porridgeMenus = [
   {
     id: 5,
-    imgUrl: '/menu/bubur/medium/bubur-cakwe-telor-campur.png',
+    imgUrl: '/images/porridge/medium/bubur-cakwe-telor-campur.png',
     name: 'Bubur Cakwe Telor Campur',
     price: 28000
   },
   {
     id: 6,
-    imgUrl: '/menu/bubur/small/bubur-sarang-telor-kepo.png',
+    imgUrl: '/images/porridge/small/bubur-sarang-telor-kepo.png',
     name: 'Bubur Telor Sarang Kepo',
     price: 28000
   },
   {
     id: 7,
-    imgUrl: '/menu/bubur/small/bubur-sarang-telor-setengah-matang.png',
+    imgUrl: '/images/porridge/small/bubur-sarang-telor-setengah-matang.png',
     name: 'Bubur Sarang Telor Setengah Matang',
     price: 35000
   },
   {
     id: 8,
-    imgUrl: '/menu/bubur/small/bubur-sarang-telor-setengah-matang.png',
+    imgUrl: '/images/porridge/small/bubur-sarang-telor-setengah-matang.png',
     name: 'Bubur Sarang Telor Setengah Matang',
     price: 28000
   },
   {
     id: 9,
-    imgUrl: '/menu/bubur/small/bubur-ayam-campur.png',
+    imgUrl: '/images/porridge/small/bubur-ayam-campur.png',
     name: 'Bubur Cakwe Telor Campur',
     price: 28000
   }
@@ -61,26 +61,26 @@ const porridgeMenus = [
 const riceMenus = [
   {
     id: 10,
-    imgUrl: '/menu/rice/nasi-goreng-hainan.png',
+    imgUrl: '/images/rice/nasi-goreng-hainan.png',
     name: 'Nasi Goreng Hainan',
     price: 28000
   },
-  { id: 11, imgUrl: '/menu/rice/nasi-bakmoy.png', name: 'Nasi Bakmoy', price: 28000 },
+  { id: 11, imgUrl: '/images/rice/nasi-bakmoy.png', name: 'Nasi Bakmoy', price: 28000 },
   {
     id: 12,
-    imgUrl: '/menu/rice/nasi-gongso.png',
+    imgUrl: '/images/rice/nasi-gongso.png',
     name: 'Nasi Gongso',
     price: 32000
   },
   {
     id: 13,
-    imgUrl: '/menu/rice/nasi-hainan.png',
+    imgUrl: '/images/rice/nasi-hainan.png',
     name: 'Nasi Hainan',
     price: 28000
   },
   {
     id: 14,
-    imgUrl: '/menu/rice/nasi-goreng-appa-yip.png',
+    imgUrl: '/images/rice/nasi-goreng-appa-yip.png',
     name: 'Nasi Goreng App Yip',
     price: 31000
   }
@@ -89,31 +89,31 @@ const riceMenus = [
 const chickenNoodleMenus = [
   {
     id: 15,
-    imgUrl: '/menu/chicken_noodle/mi-ayam.png',
+    imgUrl: '/images/chicken_noodle/mi-ayam.png',
     name: 'Mi Ayam',
     price: 25000
   },
   {
     id: 16,
-    imgUrl: '/menu/chicken_noodle/mi-ayam-telor-orak-arik.png',
+    imgUrl: '/images/chicken_noodle/mi-ayam-telor-orak-arik.png',
     name: 'Mi Ayam Telor Orak Arik',
     price: 28000
   },
   {
     id: 17,
-    imgUrl: '/menu/chicken_noodle/mi-ayam-telur.png',
+    imgUrl: '/images/chicken_noodle/mi-ayam-telur.png',
     name: 'Mi Ayam Telur',
     price: 30000
   },
   {
     id: 18,
-    imgUrl: '/menu/chicken_noodle/mi-ayam-telor-orak-arik.png',
+    imgUrl: '/images/chicken_noodle/mi-ayam-telor-orak-arik.png',
     name: 'Mi Ayam Telor Orak Arik',
     price: 28000
   },
   {
     id: 19,
-    imgUrl: '/menu/chicken_noodle/mi-ayam-ceker.png',
+    imgUrl: '/images/chicken_noodle/mi-ayam-ceker.png',
     name: 'Mi Ayam Ceker',
     price: 28000
   }
@@ -122,31 +122,31 @@ const chickenNoodleMenus = [
 const snackMenus = [
   {
     id: 20,
-    imgUrl: '/menu/snack/risoles-roti.png',
+    imgUrl: '/images/snack/risoles-roti.png',
     name: 'Risoles Roti',
     price: 10000
   },
   {
     id: 21,
-    imgUrl: '/menu/snack/pastel-kare-ayam.png',
+    imgUrl: '/images/snack/pastel-kare-ayam.png',
     name: 'Pastel Kare Ayam',
     price: 10000
   },
   {
     id: 22,
-    imgUrl: '/menu/snack/bakso-ayam-goreng.png',
+    imgUrl: '/images/snack/bakso-ayam-goreng.png',
     name: 'Bakso Ayam Goreng',
     price: 8000
   },
   {
     id: 23,
-    imgUrl: '/menu/snack/siomay-ayam.png',
+    imgUrl: '/images/snack/siomay-ayam.png',
     name: 'Siomay Ayam',
     price: 10000
   },
   {
     id: 24,
-    imgUrl: '/menu/snack/cakwe.png',
+    imgUrl: '/images/snack/cakwe.png',
     name: 'Cakwe',
     price: 6000
   }
@@ -155,31 +155,31 @@ const snackMenus = [
 const drinkMenus = [
   {
     id: 25,
-    imgUrl: '/menu/drink/es-teh.png',
+    imgUrl: '/images/drink/es-teh.png',
     name: 'Es Teh',
     price: 5000
   },
   {
     id: 26,
-    imgUrl: '/menu/drink/milo.png',
+    imgUrl: '/images/drink/milo.png',
     name: 'Milo',
     price: 10000
   },
   {
     id: 27,
-    imgUrl: '/menu/drink/es-jeruk.png',
+    imgUrl: '/images/drink/es-jeruk.png',
     name: 'Es Jeruk',
     price: 7000
   },
   {
     id: 28,
-    imgUrl: '/menu/drink/teh-susu.png',
+    imgUrl: '/images/drink/teh-susu.png',
     name: 'Teh Susu',
     price: 16000
   },
   {
     id: 29,
-    imgUrl: '/menu/drink/es-kelapa.png',
+    imgUrl: '/images/drink/es-kelapa.png',
     name: 'Es Kelapa',
     price: 12000
   }
