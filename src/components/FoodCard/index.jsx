d8b2590512cf941802d943fd32ba994302e09ed8
Gerrad <gerrad.natanael@ui.ac.id>
2022-09-09 12:58:39 +0700

    add new context and function to show detail menu

--- src/components/FoodCard/index.jsx
+++ src/components/FoodCard/index.jsx
@@ -4,37 +4,48 @@ import RiceMenu from './RiceMenu';
 import ChickenNoodleMenu from './ChickenNoodleMenu';
 import SnackMenu from './SnackMenu';
 import DrinkMenu from './DrinkMenu';
+import ShowDetail from '../ShowDetail';
 import { Row } from 'react-bootstrap';
+import MenuContextProvider, { useMenu } from '../../contexts/menuContext';
 
 const FoodCard = () => {
-    return (
-        <main className='menu'>
+  const { menuExpanded, setMenuExpanded, isOpen, setIsOpen } = useMenu();
+  const showDetail = function (menu) {
+    setMenuExpanded(menu);
+    setIsOpen(true);
+  };
+
+  return (
+    <>
+      <ShowDetail menu={menuExpanded} isOpen={isOpen} onHide={setIsOpen} />
+      <main className="menu">
         <Row>
-            <p className='text-2xl text-weight-medium p-0 text-menu'>Recommendation</p>
-            <RecommendedMenu />
+          <p className="text-2xl text-weight-medium p-0 text-menu">Recommendation</p>
+          <RecommendedMenu onClick={showDetail} />
         </Row>
         <Row>
-            <p className='text-2xl text-weight-medium p-0 text-menu'>Bubur</p>
-            <PorridgeMenu />
+          <p className="text-2xl text-weight-medium p-0 text-menu">Bubur</p>
+          <PorridgeMenu onClick={showDetail} />
         </Row>
         <Row>
-            <p className='text-2xl text-weight-medium p-0 text-menu'>Nasi</p>
-            <RiceMenu />
+          <p className="text-2xl text-weight-medium p-0 text-menu">Nasi</p>
+          <RiceMenu onClick={showDetail} />
         </Row>
         <Row>
-            <p className='text-2xl text-weight-medium p-0 text-menu'>Mi Ayam</p>
-            <ChickenNoodleMenu />
+          <p className="text-2xl text-weight-medium p-0 text-menu">Mi Ayam</p>
+          <ChickenNoodleMenu onClick={showDetail} />
         </Row>
         <Row>
-            <p className='text-2xl text-weight-medium p-0 text-menu'>Snack</p>
-            <SnackMenu />
+          <p className="text-2xl text-weight-medium p-0 text-menu">Snack</p>
+          <SnackMenu onClick={showDetail} />
         </Row>
         <Row>
-            <p className='text-2xl text-weight-medium p-0 text-menu'>Drink</p>
-            <DrinkMenu/>
+          <p className="text-2xl text-weight-medium p-0 text-menu">Drink</p>
+          <DrinkMenu onClick={showDetail} />
         </Row>
-    </main>
-    )
-}
+      </main>
+    </>
+  );
+};
 
 export default FoodCard;
