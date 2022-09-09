a156146b3cae780e698bf28b30784b37bc64c6f2
Tata Sulysta <tata_ss@softwareseni.com>
2022-09-09 09:49:57 +0700

    hide breadcrumbs and refractor navbar code

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,9 +3,9 @@ import { navItem } from '../../utils/navItem';
 import NavTab from '../NavTab';
 import styles from './styles.module.css';
 
-const Navbar = (props) => {
+const Navbar = () => {
   return (
-    <nav className={`${styles.navbar} ${props.className}`}>
+    <nav className={styles.navbar}>
       <ul>
         {navItem.map((item) => {
           return <NavTab data={item} key={item.id} />;
