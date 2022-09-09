4d0f13d4d6d12f095cb33ccdf5839c2d2d7354c8
Gerrad <gerrad.natanael@ui.ac.id>
2022-09-09 09:36:21 +0700

    show cart detail

--- src/components/UI/Kart/index.jsx
+++ src/components/UI/Kart/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './styles.module.css';
+import { useCart } from '../../../contexts/Context';
 
 function Kart(props) {
+  const { setIsCartOpen: setShow } = useCart();
   return (
-    <a className={styles.kart} onClick={props.onClick}>
+    <a className={styles.kart} onClick={() => setShow(true)}>
       <img src="kart.svg" alt="Kart Icon" className={styles.icon} />
     </a>
   );
